689e16d2628ffa222bf6673e3405031368c98dd5
loganballard <33067676+loganballard@users.noreply.github.com>
2018-12-01 11:28:45 -0800

    refactoring redditbot into its own file

--- code/redditBot.py
+++ code/redditBot.py
@@ -3,13 +3,11 @@ import praw, pprint
 
 SUBR = "cs510bottesting" #environ['SUBREDDIT']
 oldPosts = set()    # post IDs that have been addressed already
-# DO COMMIT THIS
 
 USERNAME = environ['BOT_USERNAME']
 PASSWORD = environ['BOT_PASSWORD']
 CLIENT_ID = environ['BOT_CLIENT_ID']
 CLIENT_SECRET = environ['BOT_CLIENT_SECRET']
-
 
 # reddit API
 def getRedditInstance():
@@ -21,31 +19,52 @@ def getRedditInstance():
 
 # returns whether or not the posted url is a image
 def isImageOrText(post):
-    if (post.post_hint == 'image') or (post.post_hint == 'text'):
-        return True
-    return False
+    if post.is_self:
+        return "txt"
+    elif (post.post_hint is not None) and (post.post_hint == 'image'):
+        return "img"
+    return None
 
 # returns a list of image posts that haven't been visited yet in 
 # the specified subreddit
-def getNewImagePosts(red):
+def getNewTextOrImagePosts(red):
     newPosts = []
     subRedInstance = red.subreddit(SUBR)
     for post in subRedInstance.new():
-        if (post.name not in oldPosts) and (isImageLink(post)):
-            newPosts.append(post)
+        #pprint.pprint(vars(post))
+        if (post.id not in oldPosts):
+            postType = isImageOrText(post)
+            if postType is not None:
+                newPosts.append((post, postType))
     return newPosts
 
+def googleTextAnalysis(contentString):
+    return []
+
+def googleImageAnalysis(url):
+    return []
+
+# returns list of strings, best guess info based on the 
+# google ML APIs.  Image gets descriptors, text gets tone
+def googleMlWrapper(postData):
+    if postData[1] == "txt":
+        return googleTextAnalysis(postData[0].selftext)
+    return googleImageAnalysis(postData[0].url)
+
 # takes the new posts and does some logic with them
+# newPosts is a list of posts, posts is a tuple
+# (post Object, postType String)
 def processNewPosts(posts):
+    postsToSend = {}
     for post in posts:
         postInfo = {}
-        postInfo['type'] = post_hint
-        url = post.url
-        # TODO
-        ## 
-        # TODO
-        print(url)
-        oldPosts.add(post.name)
+        postInfo['type'] = post[1]
+        postInfo['title'] = post[0].title
+        postInfo['url'] = post[0].url
+        postInfo['wordList'] = googleMlWrapper(post)
+        postsToSend[post[0].id] = postInfo
+        oldPosts.add(post[0].id)
+    return postsToSend
 
 # main logic
 #   Called from outside the module, gets an instance of reddit,
@@ -53,11 +72,14 @@ def processNewPosts(posts):
 #   and returns a dict of postID -> {
 #                                       'type': 'image' or 'text',
 #                                       'url': url,
-#                                       'descriptors': [wordList]
+#                                       'wordList': [list of descriptors]
 #                                   }
 #   where wordList is either a list of image descriptors (image post)
 #   or a list of words describing the tone of the text (text post)
 def getNewPostInfo():
     r = getRedditInstance()
-    np = getNewImagePosts(r)
+    np = getNewTextOrImagePosts(r)
     return processNewPosts(np)
+
+d = getNewPostInfo()
+pprint.pprint(d)
