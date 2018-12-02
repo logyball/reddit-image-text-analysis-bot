1a457ee73c2b8c52e795938bb7874c63dd83b6c5
Jordan <jsd9@pdx.edu>
2018-12-01 16:14:53 -0800

    spotify query function

---
+++ query_spotify.py
@@ -0,0 +1,19 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import json
+from os import environ
+import random
+CLIENT_ID = 'd3d39178dcd344c1b1eb1933447ecffd'
+CLIENT_SECRET = 'de9d83475e2b4f3897e8be424d26a6f7'
+#client_id=environ[CLIENT_ID]
+#client_secret=environ[CLIENT_SECRET]
+
+def get_a_playlist(query):
+    offset = random.randint(1,20)
+    credentials = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    spotify = spotipy.Spotify(client_credentials_manager=credentials)
+    results = spotify.search(query,limit=1, offset=offset,type='playlist')
+    link = results['playlists']['items'][0]['external_urls']['spotify']
+    return link
+
+print(get_a_playlist('nervous'))
