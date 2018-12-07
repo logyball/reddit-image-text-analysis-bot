d9d7304aee74dd4cf65fe586438f197eab915b70
loganballard <33067676+loganballard@users.noreply.github.com>
2018-12-06 20:36:26 -0800

    reverse chronological order

--- code/front_end.py
+++ code/front_end.py
@@ -14,8 +14,8 @@ def index():
     Page that displays the actions of the bot
     """
     actions = [dict(post_id=row[0], action=row[1], time=row[2], link=row[4]) for row in db.select()]
-    
-    for i in actions[::-1]:
+    actions.reverse() # reverse chronological order
+    for i in actions:
         i['time'] = time.gmtime(i['time'])
         timeString = time.strftime('%m/%d/%Y -- %H:%M:%S', i['time'] )
         i['time'] = timeString
